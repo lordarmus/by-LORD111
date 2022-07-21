b4459a2820bcd354a7e2514b8d1364479af27048
Ikhsan5547 <101627472+Ikhsan5547@users.noreply.github.com>
2022-07-21 15:48:43 +0800

    Update jadibot.js

--- plugins/jadibot.js
+++ plugins/jadibot.js
@@ -66,7 +66,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     global.conns.push(conn)
   } else throw 'Tidak bisa membuat bot didalam bot!\n\nhttps://wa.me/' + global.conn.user.jid.split`@`[0] + '?text=.jadibot'
 }
-handler.help = ['tools']
+handler.help = ['jadibot']
 handler.tags = ['jadibot']
 
 handler.command = /^jadibot$/i
