860709e22745e86e5577bc42489aa4aa62df4c0c
Ikhsan5547 <101627472+Ikhsan5547@users.noreply.github.com>
2022-07-21 22:28:19 +0800

    Add files via upload

---
+++ plugins/meme_indo.js
@@ -0,0 +1,15 @@
+let fetch = require('node-fetch')
+let handler = async (m, { text }) => {
+  let url = await fetch('https://api.zeks.xyz/api/memeindo?apikey=caliph_71')
+  let barbar = await url.json()
+conn.sendFile(m.chat, barbar.result, 'memeindo', JSON.stringify(barbar, null, 2), m)
+}
+handler.help = ['meme-indo']
+handler.tags = ['fun']
+
+handler.command = /^(memeindo)$/i
+handler.register = true
+handler.private = false
+handler.limit = true
+
+module.exports = handler
