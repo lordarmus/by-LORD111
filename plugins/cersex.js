6ade2af048e92f06a0eaa6e734b19f5e646d4290
Ikhsan5547 <101627472+Ikhsan5547@users.noreply.github.com>
2022-07-22 21:39:54 +0800

    Add files via upload

--- plugins/cersex.js
+++ plugins/cersex.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
-
+
+
 
 async function fetchJson(url) {
 ffs = await fetch(url)
@@ -7,23 +8,40 @@ js = await ffs.json()
 return js
 }
 
- let handler  = async (m, { conn }) => {
+ let handler  = async (m, { conn }) => {
+
       ss = await fetchJson('https://leyscoders-api.herokuapp.com/api/cersex?apikey=MIMINGANZ')
-      conn.sendFile(m.chat, ss.gambar, '', `Cerita : ${ss.result}`.trim(), m)
-}
-handler.help = ['cersex']
-handler.tags = ['dewasa']
-handler.command = /^(cersex)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = false
-handler.group = false
-handler.private = false
-
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
+      conn.sendFile(m.chat, ss.gambar, '', `Cerita : ${ss.result}`.trim(), m)
+
+}
+
+handler.help = ['cersex']
+
+handler.tags = ['dewasa']
+
+handler.command = /^(cersex)$/i
+
+handler.owner = false
+
+handler.mods = false
+
+handler.premium = true
+
+handler.group = false
+
+handler.private = false
+
+
+
+handler.admin = false
+
+handler.botAdmin = false
+
+
+
+handler.fail = null
+
 handler.limit = true
-
+
+
 module.exports = handler
